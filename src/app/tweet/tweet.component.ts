82f5d13f148387f4fbc81a3709faf32a44a6ced5
Ken Mazaika <kmazaika@gmail.com>
2017-05-08 14:35:58 -0700

    Refactor tweet component a little bit.

--- src/app/tweet/tweet.component.ts
+++ src/app/tweet/tweet.component.ts
@@ -6,34 +6,41 @@ import { Input } from '@angular/core';
   styleUrls: ['./tweet.component.css']
 })
 export class TweetComponent implements OnInit {
-    @Input() text: string;
+  @Input() text: string;
+
   constructor() { }
+  ngOnInit() { }
 
-  ngOnInit() {
+  // Parse the tweet into an array of words.  Special words, like
+  // hashtags and mentions have a special _type_.
+  // Normal words have no type.
+  //
+  // examples:
+  // { value: "Hello" }
+  // { type: "mention", value: "@kenmazaika" }
+  // { type: "hashtag", value: "#nba" }
+  parseTweet() {
+    return this.text.split(" ").map( (str) => {
+  		if( str[0] === "@" ) {
+  			return {
+  				type: "mention",
+  				value: str
+  			};
+  		}
+  		else if( str[0] === "#" ) {
+  			return {
+  				type: "hashtag",
+  				value: str
+  			};
+  		}
+  		return { value: str };
+  	});
   }
 
-  someMethod() {
-
-	const val = this.text.split(" ").map((str) => {
-
-		if(str[0] === "@") {
-			return {
-				type: "mention",
-				value: str
-			};
-		}
-		else if(str[0] === "#") {
-			return {
-				type: "hashtag",
-				value: str
-			};
-		}
-		return {value: str};
-	});
-
-	return val;
-  }
-
+  // Removes a colon from the string if it's the last letter of the
+  // word.  This fixes the common problem with retweets:
+  // RT @kenmazaika: Here's what I think.
+  // Where the twitter account should be @kenmazaika, not @kenmazaika:
   stripColon(val: string) {
   	const lastChar = val[val.length - 1];
   	if(lastChar === ":") {
@@ -44,8 +51,8 @@ export class TweetComponent implements OnInit {
   	}
   }
 
-  stripHashtag(val) {
-
+  // Remove the # from the beginning of a hashtag if it's present.
+  stripHashtag(val: string) {
   	if(val[0] === "#") {
   		return val.substring(1, val.length);
   	}
@@ -54,8 +61,10 @@ export class TweetComponent implements OnInit {
   	} 
   }
 
+  // A helper method to show the stripped trailing colon that
+  // was removed from the twitter user's name.
   trailingColon(val: string) {
-	const lastChar = val[val.length - 1];
+	  const lastChar = val[val.length - 1];
   	if(lastChar === ":") {
   		return ":";
   	}
